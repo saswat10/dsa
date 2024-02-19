36609ebdafbed7e72be5683db77b9d9b488a25cd
Saswat Samal <saswat1091@outlook.com>
2024-02-19 22:33:58 +0530

    tree data structure

---
+++ Data_Structures/tree.py
@@ -0,0 +1,55 @@
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.children = []
+    self.parent = None
+
+  def addChild(self, child):
+    child.parent = self
+    self.children.append(child)
+
+  def get_level(self):
+    level = 0
+    p = self.parent
+    while p:
+      level += 1
+      p = p.parent
+    
+    return level
+  
+  def print_tree(self):
+    spaces = ' ' * self.get_level() * 3
+    prefix = spaces + "|__" if self.parent else ""
+    print(prefix + self.data)
+    if self.children:
+      for child in self.children:
+        child.print_tree()
+
+def build_product_tree():
+  root = TreeNode('Electronics')
+
+
+  laptop = TreeNode('Laptop')
+  laptop.addChild(TreeNode('surfacebook'))
+  laptop.addChild(TreeNode('pangolin'))
+  laptop.addChild(TreeNode('macbook'))
+
+  mobile = TreeNode('Mobile')
+  mobile.addChild(TreeNode('iPhone'))
+  mobile.addChild(TreeNode('pixel'))
+  mobile.addChild(TreeNode('Vivo'))
+
+  tv = TreeNode('TV')
+  tv.addChild(TreeNode('Samsung'))
+  tv.addChild(TreeNode('Bravia'))
+  
+  root.addChild(laptop)
+  root.addChild(tv)
+  root.addChild(mobile)
+
+  return root
+
+if __name__ == '__main__':
+  root = build_product_tree()
+  root.print_tree()
+  pass
